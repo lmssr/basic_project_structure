fc58d5b4af86873590556b0f2f02c96da6ccc91b
Luc Mosser <luc.mosser86@orange.fr>
2019-11-14 15:41:52 +0100

    playing with textures

---
+++ js/app.js
@@ -0,0 +1,128 @@
+  // these need to be accessed inside more than one function so we'll declare them first
+  let container;
+  let camera;
+  let renderer;
+  let scene;
+  let mesh;
+
+  function init() {
+
+    container = document.querySelector( '#scene-container' );
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x8FBCD4 );
+
+    createCamera();
+    createLights();
+    createMeshes();
+    createRenderer();
+
+    renderer.setAnimationLoop( () => {
+
+      update();
+      render();
+
+    } );
+
+  }
+
+  function createCamera() {
+
+    camera = new THREE.PerspectiveCamera(
+      35, // FOV
+      container.clientWidth / container.clientHeight, // aspect
+
+      0.1, // near clipping plane
+      100, // far clipping plane
+    );
+
+    camera.position.set( 0, 0, 10 );
+
+  }
+
+  function createLights() {
+
+    // Create a directional light
+    const light = new THREE.DirectionalLight( 0xffffff, 3.0 );
+
+    // move the light back and up a bit
+    light.position.set( 10, 10, 10 );
+
+    // remember to add the light to the scene
+    scene.add( light );
+
+  }
+
+  function createMeshes() {
+
+    const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
+
+    const textureLoader = new THREE.TextureLoader();
+
+    const texture = textureLoader.load( 'textures/uv_test_bw.png' );
+
+    texture.encoding = THREE.sRGBEncoding;
+    texture.anisotropy = 16;
+
+    const material = new THREE.MeshStandardMaterial( {
+      map: texture,
+    } );
+
+    mesh = new THREE.Mesh( geometry, material );
+
+    scene.add( mesh );
+
+  }
+
+  function createRenderer() {
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( container.clientWidth, container.clientHeight );
+
+    renderer.setPixelRatio( window.devicePixelRatio );
+
+    renderer.gammaFactor = 2.2;
+    renderer.gammaOutput = true;
+
+    container.appendChild( renderer.domElement );
+
+  }
+
+
+  // perform any updates to the scene, called once per frame
+  // avoid heavy computation here
+  function update() {
+
+    // increase the mesh's rotation each frame
+    mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+
+  }
+
+  // render, or 'draw a still image', of the scene
+  function render() {
+
+    renderer.render( scene, camera );
+
+  }
+
+  // a function that will be called every time the window gets resized.
+  // It can get called a lot, so don't put any heavy computation in here!
+  function onWindowResize() {
+
+    // set the aspect ratio to match the new browser window aspect ratio
+    camera.aspect = container.clientWidth / container.clientHeight;
+
+    // update the camera's frustum
+    camera.updateProjectionMatrix();
+
+    // update the size of the renderer AND the canvas
+    renderer.setSize( container.clientWidth, container.clientHeight );
+
+  }
+
+  window.addEventListener( 'resize', onWindowResize );
+
+  // call the init function to set everything up
+  init();
